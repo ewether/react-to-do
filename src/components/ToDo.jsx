ba87a5b2d76c6b3a4bd3e666c52b0dfbb1cf7fb9
Elise Wetherholt <ewether@gmail.com>
2021-03-19 09:08:17 -0400

    added green toggle to todos

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+
+// on todo click -> turn green
 
 function ToDo() {
+    const [green, setGreen] = useState(false);
+
     return (
-        <div className="todo">
+        <div className={` todo ${green ? 'green' : 'white'} `} onClick={() => setGreen(!green)}>
             <p>Walk dog</p>
         </div>
     )
