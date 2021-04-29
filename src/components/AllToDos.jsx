c37382683c43e8155f82449c21575af19106cb02
Elise Wetherholt <ewether@gmail.com>
2021-04-29 13:01:35 -0400

    added plus icon with animation, added checkmark and delete icons with hover effects

--- src/components/AllToDos.jsx
+++ src/components/AllToDos.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ToDo from './ToDo'
+import AddTask from "../utilities/icons/add-task.png";
 
 function AllToDos() {
     const [value, setValue] = useState('');
@@ -24,25 +25,31 @@ function AllToDos() {
     }
 
     return (
-        <div className="main-wrapper">
-            <h1>Tasks</h1>
+      <div className="main-wrapper">
+        <h1>Tasks</h1>
 
-            <div className="new-todo">
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        <input value={value} onChange={handleChange} type="text" name="todo" placeholder="Enter a new task ..." />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-
-            <section className="todos-wrapper">
-                {todos.map((todo) => (
-                    <ToDo text={todo} />
-                ))}
-            </section>
+        <section className="todos-wrapper">
+          {todos.map((todo) => (
+            <ToDo text={todo} />
+          ))}
+        </section>
+        <div className="new-todo">
+          <img className="add-task-icon" src={AddTask} alt="Add task" />
+          <form onSubmit={handleSubmit}>
+            <label>
+              <input
+                value={value}
+                onChange={handleChange}
+                type="text"
+                name="todo"
+                placeholder="Enter a new task ..."
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
-    )
+      </div>
+    );
 }
 
 export default AllToDos;
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import Checkmark from "../utilities/icons/checkmark.png";
+import GreyCheckmark from "../utilities/icons/grey-checkmark.png";
+import GreyDelete from "../utilities/icons/grey-delete.png";
+import Delete from "../utilities/icons/delete.png";
 
 // on todo click -> turn green
 
 function ToDo({ text }) {
     const [complete, setComplete] = useState(false);
     const [deleted, setDeleted] = useState(false);
+    const [hoverDelete, setHoverDelete] = useState(false);
  
     function deleteClick() {
         setDeleted(true);
@@ -22,9 +27,26 @@ function ToDo({ text }) {
             <p>{text}</p>
 
             <div className="todo-buttons">
-              <button className="delete-button" onClick={() => deleteClick()}>
-                X
-              </button>
+              {/* <div className="checkmark-wrapper"> */}
+              {complete ? (
+                <img className="checkmark" src={Checkmark} alt="checkmark" />
+              ) : (
+                <img
+                  className="checkmark grey-checkmark"
+                  src={GreyCheckmark}
+                  alt="grey checkmark"
+                />
+              )}
+              {/* </div> */}
+              <img
+                className="delete-button"
+                src={GreyDelete}
+                alt="Delete"
+                onClick={() => deleteClick()}
+                onMouseOver={e => (e.currentTarget.src = Delete)}
+                onMouseOut={e => (e.currentTarget.src = GreyDelete)
+                }
+              />
             </div>
           </div>
         )}
