ede72fbe8a645035a4df729d5c2f8950437b5ca8
Elise Wetherholt <ewether@gmail.com>
2021-04-30 11:56:10 -0400

    added hover effects to icons/text on todo

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -2,17 +2,23 @@ import React, { useState } from "react";
 import Checkmark from "../utilities/icons/checkmark.png";
 import GreyCheckmark from "../utilities/icons/grey-checkmark.png";
 import GreyDelete from "../utilities/icons/grey-delete.png";
-import Delete from "../utilities/icons/delete.png";
+import RedDelete from "../utilities/icons/red-delete.png";
+import LtRedDelete from "../utilities/icons/ltred-delete.png";
+import LtGrnCheckmark from "../utilities/icons/ltgrn-checkmark.png";
 
 // on todo click -> turn green
 
 function ToDo({ text }) {
     const [complete, setComplete] = useState(false);
     const [deleted, setDeleted] = useState(false);
-    const [hoverDelete, setHoverDelete] = useState(false);
+    const [hover, setHover] = useState(false);
  
     function deleteClick() {
         setDeleted(true);
+    }
+
+    function todoHover() {
+      setHover(!hover);
     }
 
     return (
@@ -23,6 +29,8 @@ function ToDo({ text }) {
           <div
             className={` todo ${complete ? "complete" : "not-complete"} `}
             onClick={() => setComplete(!complete)}
+            onMouseOver={() => todoHover()}
+            onMouseOut={() => todoHover()}
           >
             <p>{text}</p>
 
@@ -33,19 +41,18 @@ function ToDo({ text }) {
               ) : (
                 <img
                   className="checkmark grey-checkmark"
-                  src={GreyCheckmark}
+                  src={hover ? LtGrnCheckmark : GreyCheckmark}
                   alt="grey checkmark"
                 />
               )}
               {/* </div> */}
               <img
-                className="delete-button"
-                src={GreyDelete}
+                className={"delete-button"}
+                src={hover ? LtRedDelete : GreyDelete}
                 alt="Delete"
                 onClick={() => deleteClick()}
-                onMouseOver={e => (e.currentTarget.src = Delete)}
-                onMouseOut={e => (e.currentTarget.src = GreyDelete)
-                }
+                onMouseOver={(e) => (e.currentTarget.src = RedDelete)}
+                onMouseOut={(e) => (e.currentTarget.src = GreyDelete)}
               />
             </div>
           </div>
